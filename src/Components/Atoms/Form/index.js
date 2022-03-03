26c25fdbe2dd0ce3c8c057d2dcaa6821443a50f3
Sara Borges <sarakborges@outlook.com>
2022-03-03 16:57:35 -0300

    fixed forms being disabled by default

--- src/Components/Atoms/Form/index.js
+++ src/Components/Atoms/Form/index.js
@@ -1,4 +1,19 @@
+// Dependencies
+import { useContext, useEffect } from "react";
+
+// Contexts
+import { AppContext } from "Contexts/App";
+
 const Form = ({ children, onSubmit }) => {
+  const { appDispatch } = useContext(AppContext);
+
+  useEffect(() => {
+    appDispatch({
+      type: "SET_IS_REQUESTING",
+      data: false,
+    });
+  }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     onSubmit();
